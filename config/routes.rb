3058ebb83cf3f19ebcffe22396ddcbb960c31ef0
satoshishimazaki <sattori06010601@gmail.com>
2014-09-28 02:08:02 +0900

    Finish store signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sample5App::Application.routes.draw do
+  resources :stores
   root  'static_pages#home'
   match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/signup',  to: 'stores#new',            via: 'get'
 end
