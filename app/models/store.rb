731dbb7e9a5d6bc19222f63d1663d9222f163977
satoshishimazaki <sattori06010601@gmail.com>
2014-09-28 02:44:09 +0900

    Finish sign in

--- app/models/store.rb
+++ app/models/store.rb
@@ -7,4 +7,18 @@ class Store < ActiveRecord::Base
 	validates :url, presence: true
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+
+    def Store.new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def Store.encrypt(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+    def create_remember_token
+      self.remember_token = Store.encrypt(Store.new_remember_token)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140927161443) do
+ActiveRecord::Schema.define(version: 20140927173356) do
 
   create_table "stores", force: true do |t|
     t.string   "name"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20140927161443) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "stores", ["email"], name: "index_stores_on_email", unique: true
+  add_index "stores", ["remember_token"], name: "index_stores_on_remember_token"
 
 end
