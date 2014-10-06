379244c23b46b9b4febf1e66b692fe97b7d715ec
satoshishimazaki <sattori06010601@gmail.com>
2014-10-07 00:52:39 +0900

    amazon

--- app/admin/store.rb
+++ app/admin/store.rb
@@ -14,5 +14,39 @@ ActiveAdmin.register Store do
   #   permitted
   # end
   
-  permit_params :name, :email, :tel, :url, :password, :password_confirmation, :image, :category
+  permit_params :name, :email, :tel, :url, :category
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :email
+    column :tel
+    column :url
+    column :category
+    column :current_sign_in_at
+    column :sign_in_count
+    column :created_at
+    default_actions
+  end
+
+  filter :name
+  filter :email
+  filter :tel
+  filter :url
+  filter :category
+  filter :current_sign_in_at
+  filter :sign_in_count
+  filter :created_at
+
+  form do |f|
+    f.inputs "Admin Details" do
+      f.input :name
+      f.input :email
+      f.input :tel
+      f.input :url
+      f.input :category
+    end
+    f.actions
+  end
 end
--- config/carrierwave.rb
+++ config/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJAPVQ3OJU7QPA2OA',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => '#',
+    :aws_secret_access_key  => '#',
     :region                 => 'ap-northeast-1',
     :host                   => 'beansstreettry1.s3-website-ap-northeast-1.amazonaws.com'
     :endpoint               => 'beansstreettry1.s3-website-ap-northeast-1.amazonaws.com'
