183685c796c8e75fd42f5a0fa79638150bcceebb
satoshishimazaki <sattori06010601@gmail.com>
2015-01-12 19:47:09 +0900

    title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # ページごとの完全なタイトルを返します。
   def full_title(page_title)
-    base_title = "BeansStreet"
+    base_title = "now here"
     if page_title.empty?
       base_title
     else
