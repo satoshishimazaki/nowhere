db72e53ea0b30cd6de3223ee67127e42a6792e5e
satoshishimazaki <sattori06010601@gmail.com>
2015-03-25 09:55:54 +0900

    reopairsearch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,12 @@
 class SearchController < ApplicationController
   def index
   	@search_word = params[:word]
-  	@stores = Store.where(['name LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
-  	@stores = Store.where(['category LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
-    @articles = Article.where(['title LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
-    @articles = Article.where(['herenowtitle LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
-    @articles = Article.where(['content LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
-    @users = User.where(['address LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+  	@stores = Store.where(['name LIKE ? OR category LIKE ?', "%#{@search_word}%", "%#{@search_word}%"]).paginate(page: params[:page])
+  	# @stores = Store.where(['category LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+    @articles = Article.where(['title LIKE ? OR content LIKE ?', "%#{@search_word}%", "%#{@search_word}%"]).paginate(page: params[:page])
+    # @articles = Article.where(['herenowtitle LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+    # @articles = Article.where(['content LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+    # @users = User.where(['address LIKE ?', "%#{@search_word}%"]).paginate(page: params[:page])
+    # raise
   end
 end
--- app/helpers/article_helper.rb
+++ app/helpers/article_helper.rb
@@ -1,9 +1,9 @@
 module ArticleHelper
   def article_image_for(image_name, option = {})
     if image_name
-     image_tag "/docs/#{image_name}", :height =>'320'
+     image_tag "/docs/#{image_name}", :height =>'320', :align => 'top'
     else
-     image_tag "noimage.gif", :height =>'320'
+     image_tag "noimage.gif", :height =>'320', :align => 'top'
     end
   end
 
