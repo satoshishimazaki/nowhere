f19724a507c4174bbef9742f6a483e3a721b6c4a
satoshishimazaki <sattori06010601@gmail.com>
2015-04-08 17:11:25 +0900

    tel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,7 +12,7 @@ module ApplicationHelper
 
   def phone_number_link(text)
     sets_of_numbers = text.scan(/[0-9]+/)
-    number = "+81-#{sets_of_numbers.join('-')}"
+    number = "+81#{sets_of_numbers.join('-')}"
     link_to text, "tel:#{number}"
   end
 
