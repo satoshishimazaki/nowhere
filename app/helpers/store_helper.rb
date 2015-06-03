1b418bd52c9c53b502ee125c35d220766c512e90
satoshishimazaki <sattori06010601@gmail.com>
2015-06-04 00:04:21 +0900

    repairprofile

--- app/helpers/store_helper.rb
+++ app/helpers/store_helper.rb
@@ -6,4 +6,12 @@ module StoreHelper
      image_tag "no_image.png", :align => 'top'
     end
   end
+
+  def show_profile_image_for(image_name, option = {})
+    if image_name
+     image_tag "/docs/#{image_name}", :align => 'top', :id => "showprofile"
+    else
+     image_tag "no_image.png", :align => 'top'
+    end
+  end
 end
